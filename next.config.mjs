01358eeba18688a4c2a60d6bf9bdf3d7e29e8fc0
mfyameen <mfyameen@gmail.com>
2025-12-07 13:21:41 -0500

    updating dependencies and upgrading nextjs to patch security issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: false, // ENABLE security linting during builds
-  },
   typescript: {
     ignoreBuildErrors: false, // ENABLE type safety during builds
   },

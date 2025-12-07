fd4710fb1eb3b397577d728d524937ba125648dc
mfyameen <mfyameen@gmail.com>
2025-12-07 13:25:17 -0500

    updating vercel config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   typescript: {
     ignoreBuildErrors: false, // ENABLE type safety during builds
   },
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
+  experimental: {
+    optimizePackageImports: ['lucide-react', '@radix-ui/react-icons'],
+  },
+  reactStrictMode: true,
+  poweredByHeader: false,
 }
 
 export default nextConfig
